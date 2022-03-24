7ac67cc7ddd50a342f13063a869794949fd216ce
PeterBedford <b8017339@my.shu.ac.uk>
2022-03-24 17:25:53 +0000

    profile app, model & migration

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+from distutils.command.upload import upload
+from email.headerregistry import Address
+from tokenize import Name
+from turtle import up
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+ user = models.OneToOneField(User, on_delete=models.CASCADE)
+ first_name = models.CharField(max_length=30)
+ last_name = models.CharField(max_length=30)
+ address = models.CharField(max_length=100)
+ City = models.CharField(max_length=30)
+ Country = models.CharField(max_length=100)
+ Photo = models.ImageField(upload_to='profile_pics')
+
+def __str__(self):
+    return str(self.user)
+
+    
